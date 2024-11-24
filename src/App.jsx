59dcae4ab033b7f9adfa85a9cb59b9c2830cb4c6
shemnduati <nduatishem@gmail.com>
2024-11-24 19:08:42 +0300

    working with the category section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Category from './components/Category/Category'
+import Category2 from './components/Category/Category2'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Navbar />
       <Hero />
       <Category />
+      <Category2 />
     </div>
   )
 }
