ee89924276a4d6b3690704696cf5a02db084a3ce
shemnduati <nduatishem@gmail.com>
2024-11-24 12:31:29 +0300

    working with the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
 
 function App() {
 
 
   return (
     <div>
-    <Navbar />
+      <Navbar />
+      <Hero />
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -81,11 +81,20 @@ const Navbar = () => {
                         </span>
                     </a>
                       {/* Dropdown links */}
-                      <div>
-                        <ul>
+                      <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white shadow-md
+                                      dark:bg-gray-900 p-2 text-black dark:text-white">
+                        <ul className="space-y-2">
                           {
                             DropdownLinks.map((data, index) => (
-                              <li key={index}>{data.name}</li>
+                              <li key={index}>
+                                <a
+                                  className="text-gray-500 hover:text-black dark:hover:text-white duration-200
+                                              inline-block w-full p-2 hover:bg-primary/20 rounded-md font-semibold"
+                                  href={data.link}
+                                >
+                                  {data.name}
+                                </a>
+                              </li>
                             ))
                           }
                         </ul>
