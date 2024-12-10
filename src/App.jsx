2a78e07d6a78f1394768d82515519175fe9fe704
shemnduati <nduatishem@gmail.com>
2024-12-10 05:26:15 +0300

    working with the banner section

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
-import Category from './components/Category/Category'
-import Category2 from './components/Category/Category2'
-import Services from './components/Services/Services'
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import Category from './components/Category/Category';
+import Category2 from './components/Category/Category2';
+import Services from './components/Services/Services';
+import Banner from './components/Banner/Banner';
+import headphone from './assets/hero/headphone.png';
+
+const BannerData = {
+  discount: "30 OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Eaque reiciendis",
+  bgColor: "#f42c37",
+};
 
 function App() {
 
@@ -18,6 +31,7 @@ function App() {
       <Category />
       <Category2 />
       <Services />
+      <Banner data={BannerData} />
     </div>
   )
 }
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Banner = ({data}) => {
+  return (
+    <div className='min--h-[550px] flex items-center justify-center py-12'>
+       <div className='container'>
+          <div  style={{ backgroundColor: data.bgColor }} className='grid grid-cols-1 md:grid-cols-3 items-center rounded-3xl text-white gap-6'>
+            {/* first col */}
+            <div className='p-6 sm:p-8'>
+              <p className='text-sm'>{data.discount}</p>
+              <h1 className='uppercase text-4xl lg:text-7xl font-bold'>{" "}{data.title}</h1>
+              <p className='text-sm'>{data.date}</p>
+            </div>
+            {/* Second col */}
+            <div className='h-full flex items-center'>
+              <img src={data.image} alt="img" className='scale-125 w-[250px] md:w-[350px] mx-auto drop-shadow-2xl object-cover'  />
+            </div>
+            {/* third col */}
+            <div className='flex flex-col justify-center gap-4 p-6 sm:p-8'>
+              <p className='font-bold texl-xl'>{data.title2}</p>
+              <p className='text-3xl sm:text-5xl font-bold'>{data.title3}</p>
+              <p className='text-sm tracking-wide leading-5'>{data.title4}</p>
+              <div>
+                <button style={{ color: data.bgColor }} className='bg-white  py-2 px-4 rounded-full'>Shop Now</button>
+              </div>
+            </div>
+          </div>
+    </div>
+    </div>
+  )
+}
+
+export default Banner
